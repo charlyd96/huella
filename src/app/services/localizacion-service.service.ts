807acecdb777753387c85dce0d69294be63ada99
charlyd96 <carlosdesiderio96@gmail.com>
2022-11-27 22:43:05 -0300

    Refactor para traer las provincias desde el backend (problema con políticas de CORS contra API de distancia)

--- src/app/services/localizacion-service.service.ts
+++ src/app/services/localizacion-service.service.ts
@@ -3,13 +3,14 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Provincia } from '../domain/models/ubicacion/Provincia';
 import { Municipio } from '../domain/models/ubicacion/Municipio';
 import { Localidad } from '../domain/models/ubicacion/Localidad';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocalizacionService {
 
-  private baseUrl: string =  'https://ddstpa.com.ar/api/'
+  private baseUrl: string =  environment.baseUrl;
   private provincias: Provincia[] = [];
   private headers: HttpHeaders = new HttpHeaders({'Authorization' : 'Bearer PdazKo/cXANtj9VMG3dgxxNs7Mdosruc0TEZg0YW+vc='});
   private OFFSET_INICIAL: number = 1;
@@ -24,43 +25,25 @@ export class LocalizacionService {
   }
 
   private internalGetProvincias() {
-    let url = this.getUrl('provincias', 1);
+    let url = this.getUrl('provincias');
     this.http.get<Provincia[]>(url, {headers: this.headers} ).toPromise()
       .then(res => this.provincias = res.map(l => <Provincia> {id: l.id, nombre: l.nombre}));
   }
 
   async getMunicipios(provinciaId: number) : Promise<Municipio[]> {
-    let municipios: Municipio[] = [];
-    for(let offset = this.OFFSET_INICIAL; ; offset++) {
-      let url = this.getUrl('municipios', offset,'provinciaId' , provinciaId);
-      let response = <Municipio[]> await this.http.get<Municipio[]>(url, {headers: this.headers}).toPromise();
-      response = response.map(l => <Municipio> {id: l.id, nombre: l.nombre})
-      municipios = municipios.concat(response);
-      if (response.length == 0 || response.length <= this.MAX_RESPONSE) {
-        break;
-      }
-    }
-    return municipios.sort((a, b) => a.nombre.localeCompare(b.nombre));
+    let url = this.getUrl('municipios','provinciaId' , provinciaId);
+    return <Municipio[]> await this.http.get<Municipio[]>(url, {headers: this.headers}).toPromise();
   }
 
-  async getLocalidades(municipioId: number) : Promise<Localidad[]> {
-    let localidades: Localidad[] = [];
-    for(let offset = this.OFFSET_INICIAL;  ; offset++) {
-      let url = this.getUrl('localidades', offset,'municipioId' , municipioId);
-      let response = await this.http.get<Localidad[]>(url, {headers: this.headers}).toPromise();
-      response = response.map(l => <Localidad> {id: l.id, nombre: l.nombre});
-      localidades = localidades.concat(response);
-      if (response.length == 0 || response.length <= this.MAX_RESPONSE) {
-        break;
-      }
-    }
-    return localidades.sort((a, b) => a.nombre.localeCompare(b.nombre));
+  async getLocalidades(municipioId: number) : Promise<Municipio[]> {
+    let url = this.getUrl('localidades','municipioId' , municipioId);
+    return <Localidad[]> await this.http.get<Localidad[]>(url, {headers: this.headers}).toPromise();
   }
 
-  private getUrl(recurso: string, offset: number, param?: string,  paramId?: number,) {
+  private getUrl(recurso: string, param?: string,  paramId?: number,) {
     if (param) {
-      return `${this.baseUrl}${recurso}?offset=${offset}&${param}=${paramId}`
+      return `${this.baseUrl}/${recurso}?${param}=${paramId}`
     }
-    return `${this.baseUrl}${recurso}?offset=${offset}`
+    return `${this.baseUrl}/${recurso}`
   }
 }
